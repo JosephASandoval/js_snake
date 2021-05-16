4a9564e00a5bee4b811665b6fd89a84f2b5a335e
JosephASandoval <josephasandoval17@gmail.com>
2021-05-16 15:05:29 -0700

    added new file and updated module settings

--- game.js
+++ game.js
@@ -1,14 +1,28 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return; // number of seconds between each move, so 2 time per second would be 1 move every 0.5 seconds
 
-  console.log("Render");
   lastRenderTime = currentTime;
+
+  update();
+  draw();
 }
 
-
 window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  drawSnake();
+}
