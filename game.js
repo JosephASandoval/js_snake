5cf49d46364bab3ff67f7f65e3632b7e8e80d979
JosephASandoval <josephasandoval17@gmail.com>
2021-05-16 20:01:02 -0700

    finished snake

--- game.js
+++ game.js
@@ -2,15 +2,27 @@ import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
 } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart.")) {
+      window.location = "/";
+    }
+    return;
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return; // number of seconds between each move, so 2 time per second would be 1 move every 0.5 seconds
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
   lastRenderTime = currentTime;
 
@@ -22,9 +34,16 @@ window.requestAnimationFrame(main);
 
 function update() {
   updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
